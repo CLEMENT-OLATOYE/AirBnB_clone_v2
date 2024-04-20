6ab0c5ad21afd6613662a88e905953cc8a4b5441
CLEMENT OLATOYE <olatoyeclement@gmail.com>
2024-04-20 09:29:11 +0000

    task done

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- Prepares a MySQL server fot the project.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON Performance_schema . * TO 'hbnb_dev'@'localhost';
